c7f8e5c9ca35823b2751b1b4fff058d71862211b
cpt-nemmo <ysfedoseev@yandex.ru>
2024-04-14 22:53:35 +0300

    added tests package for teams

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,11 @@
-import asyncio
-from typing import AsyncGenerator
-
 import pytest
-from fastapi.testclient import TestClient
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 
 from src.main import app
 
 
-# client = TestClient(app)
-
-
 @pytest.fixture(scope="session")
-async def ac() -> AsyncGenerator[AsyncClient, None]:
-    async with AsyncClient(app=app, base_url="http://test") as ac:
+async def ac():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         yield ac
 
-
-@pytest.fixture(scope="session")
-def event_loop():
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
