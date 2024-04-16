d93177e4a7321c04cf5e86ec456e8b1a83d167da
Artem Golovachev <artbryansk94@gmail.com>
2024-04-16 14:30:44 +0300

    added some test

--- tests/api/refs/test_routers.py
+++ tests/api/refs/test_routers.py
@@ -16,11 +16,37 @@ async def test_refs_tnt(fixture_client):
 
 
 @pytest.mark.asyncio(scope="session")
+async def test_refs_tnt_filter(fixture_client):
+    idref = 166
+    season = '2022-2023'
+    response = await fixture_client.get(f"/api/v1/refs/{idref}?season=2022-2023", headers={'Last-updated': '20213111'})
+    assert response.status_code == 200
+    assert response.json()[0].get('idref') == idref
+    assert response.json()[0].get('season') == season
+    assert response.json()[0].get('tnt_id') is not None
+    assert response.json()[0].get('schedule_id') is None
+    assert response.json()[0].get('f2') is not None
+
+
+@pytest.mark.asyncio(scope="session")
 async def test_refs_games(fixture_client):
     idref = 166
     response = await fixture_client.get(f"/api/v1/refs/{idref}/games", headers={'Last-updated': '20213111'})
     assert response.status_code == 200
     assert response.json()[0].get('idref') == idref
+    assert response.json()[0].get('tnt_id') is not None
+    assert response.json()[0].get('schedule_id') is not None
+    assert response.json()[0].get('f2') is not None
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_refs_games_filter(fixture_client):
+    idref = 166
+    season = '2022-2023'
+    response = await fixture_client.get(f"/api/v1/refs/{idref}/games?season=2022-2023", headers={'Last-updated': '20213111'})
+    assert response.status_code == 200
+    assert response.json()[0].get('idref') == idref
+    assert response.json()[0].get('season') == season
     assert response.json()[0].get('tnt_id') is not None
     assert response.json()[0].get('schedule_id') is not None
     assert response.json()[0].get('f2') is not None
