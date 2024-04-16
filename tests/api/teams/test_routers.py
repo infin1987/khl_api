6a455b8339c35894e42a8803211dffd1bafd9030
cpt-nemmo <ysfedoseev@yandex.ru>
2024-04-16 14:51:37 +0300

    fixed conftest

--- tests/api/teams/test_routers.py
+++ tests/api/teams/test_routers.py
@@ -2,8 +2,8 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_get_specific_operations(ac):
-    response = await ac.get("/api/v1/players/stats",
-                            headers={'Last-updated': '20213111'})
+async def test_get_specific_operations(fixture_client):
+    response = await fixture_client.get("/api/v1/players/stats",
+                                        headers={'Last-updated': '20213111'})
 
     assert response.status_code == 200
--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,6 @@ from src.main import app
 
 
 @pytest.fixture(scope="session")
-async def ac():
+async def fixture_client():
     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
         yield ac
